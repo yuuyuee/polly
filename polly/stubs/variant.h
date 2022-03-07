61c7333c7eae6e8bbb1db6387fa34e19d6e086a5
Yu Yue <yuyue2200@hotmail.com>
2022-03-07 09:16:44 +0800

    update

--- polly/stubs/variant.h
+++ polly/stubs/variant.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2022 The Polly Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // This header file defines an `polly::variant` type for holding a type-safe
 // value of some prescribed set of types (noted as alternative types), and
 // associated functions for managing variants.
